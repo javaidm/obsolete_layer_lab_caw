9bd8d9a82edab86156f71ee79a791c9e2e454103
Mahmood Javaid <mahmood.javaid@colorado.edu>
2019-09-17 11:27:34 -0600

    Remove code not needed for somatic workflow

--- lib/bash_templates/genotype_gvcf.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-set -exuo pipefail
-gatk GenotypeGVCFs \
-    -R $REF_FASTA \
-    -O $out_file  \
-    -V gendb://${genomics_db_workspace}
--- lib/bash_templates/run_csq.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-set -exuo pipefail
-bcftools norm -m- -Ov  -f $REF_FASTA -w 10000 $vcf |\
-bcftools csq -s - --ncsq 40 -g $ENSEMBL_GENE_ANNOTATIONS -l -f $REF_FASTA -Ob -o $out_file
